5e7ad0b34c36bfae33d2666a093eb082e60e5236
Metanonia <metanonia@outlook.kr>
2022-11-11 19:54:24 +0900

    ..

--- src/main/java/com/metanonia/springmongosample/config/MongoConfig.java
+++ src/main/java/com/metanonia/springmongosample/config/MongoConfig.java
@@ -1,5 +1,7 @@
 package com.metanonia.springmongosample.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import lombok.extern.slf4j.Slf4j;
@@ -16,17 +18,12 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 public class MongoConfig {
     @Value("${spring.data.mongodb.uri}")
     private String url;
-    @Value("${spring.data.mongodb.database}")
-    private String database;
 
-    @Bean
-    public MongoClient mongoClient() {
-        return MongoClients.create(url);
-    }
 
     @Bean
     public MongoDatabaseFactory mongoDatabaseFactory() {
-        return new SimpleMongoClientDatabaseFactory(mongoClient(), database);
+        final ConnectionString connectionString = new ConnectionString(url);
+        return new SimpleMongoClientDatabaseFactory(connectionString);
     }
 
     @Bean
