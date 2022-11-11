da8e686ea5ba47d0b7c7f9abc5f5692d773344cc
Metanonia <metanonia@outlook.kr>
2022-11-11 19:45:14 +0900

    ..

--- src/main/java/com/metanonia/springmongosample/config/MongoConfig.java
+++ src/main/java/com/metanonia/springmongosample/config/MongoConfig.java
@@ -4,6 +4,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
@@ -13,15 +14,19 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 @Slf4j
 @Configuration
 public class MongoConfig {
+    @Value("${spring.data.mongodb.uri}")
+    private String url;
+    @Value("${spring.data.mongodb.database}")
+    private String database;
 
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb://localhost:27017");
+        return MongoClients.create(url);
     }
 
     @Bean
     public MongoDatabaseFactory mongoDatabaseFactory() {
-        return new SimpleMongoClientDatabaseFactory(mongoClient(), "sample");
+        return new SimpleMongoClientDatabaseFactory(mongoClient(), database);
     }
 
     @Bean
--- src/main/java/com/metanonia/springmongosample/repostory/TemplateRepository.java
+++ src/main/java/com/metanonia/springmongosample/repostory/TemplateRepository.java
@@ -1,7 +1,6 @@
 package com.metanonia.springmongosample.repostory;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.ExecutableInsertOperation;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Repository;
 
